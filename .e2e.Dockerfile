fe34f4a8a61a213b30cce230c983f9064a4e2801
Mike Valeriano <186238078+mkvlrn@users.noreply.github.com>
2025-08-25 10:19:36 -0300

    chore: give up on playwright

--- .e2e.Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM mcr.microsoft.com/playwright:latest
-
-WORKDIR /tests
-
-RUN npm init -y
-RUN npm i @playwright/test
-RUN npx playwright install --with-deps
-
-CMD ["npx", "playwright", "test"]
--- e2e/tests/smoke.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("should show login page", async ({ page }) => {
-  await page.goto("/");
-  await expect(page.getByText("Hey!")).toBeVisible();
-});
